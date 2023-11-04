d4fedff32eee7cd65ac0b6f77ee6d6b67ccfa878
Diego Salas <Diego.Salas@promart.pe>
2023-11-04 02:34:04 -0500

    Scrape league info

--- src/cron/scrape.ts
+++ src/cron/scrape.ts
@@ -1,1 +1,19 @@
+import { ApiFootballService } from "../services/api-football.service.ts";
+import { writeFile } from "../utils/file.ts";
+
 console.log('I am the scrape cron job')
+
+const LEAGUE_ID = 34
+
+const getLeague = async () => {
+    const apiFootball = new ApiFootballService()
+    const result = await apiFootball.main('leagues', { id: LEAGUE_ID })
+
+    if (result && result.length > 0) {
+        await writeFile('leagues/league.json', result[0])
+    }
+}
+
+await getLeague()
+
+console.log('Job finished')
--- src/services/league.service.ts
+++ src/services/league.service.ts
@@ -1,26 +1,12 @@
-import { readFile, writeFile } from "../utils/file.ts"
-import { ApiFootballService } from "./api-football.service.ts";
+import { readFile } from "../utils/file.ts"
 
 export class GetLeagueService {
-  private readonly leagueId: number = 34
-
   public async main() {
     try {
       const league = await readFile("leagues/league.json")
       return league
-    } catch (_) {
-      return this.getLeague()
-    }
-  }
-
-  private async getLeague() {
-    const apiFootball = new ApiFootballService()
-    const result = await apiFootball.main('leagues', { id: this.leagueId })
-
-    if (result && result.length > 0) {
-      await writeFile('leagues/league.json', result[0])
-  
-      return result[0]
+    } catch (e) {
+      console.error(e)
     }
   }
 }
